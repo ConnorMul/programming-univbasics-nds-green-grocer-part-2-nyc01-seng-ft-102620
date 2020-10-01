a7ffee1c37dfa0fc982656e756dc569fe9b50db8
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 20:18:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,7 +5,7 @@ def apply_coupons(cart, coupons)
   
   cart.each do |items|
    coupon_item = find_item_by_name_in_collection(items[:item], new_cart)
-   if 
+   if coupon_item
 end
 
 def apply_clearance(cart)
